2bfedd755169bb20351b67e831aad0f8f2c2c67e
Luka Natsvlishvili <luka.natsvlishvili17@gmail.com>
2024-06-02 18:25:54 +0200

    menu builder category CRUD

--- vendor/urls.py
+++ vendor/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path, include
 from . import views
 from accounts import views as AccountViews
+from vendor.views import menu_builder
 
 
 urlpatterns = [
     path('', AccountViews.vendorDashboard, name='vendor'),
     path('profile/', views.vprofile, name='vprofile'),
+    path('menu-builder/', views.menu_builder, name='menu_builder'),
+    path('menu-builde/category/<int:pk>/', views.fooditems_by_category, name='fooditems_by_category'),
+
+    # Category CRUD-
+    path('menu-builder/category/add', views.add_category, name='add_category'),
+    path('menu-builder/category/edit/<int:pk>/', views.edit_category, name='edit_category'),
+    path('menu-builder/category/delete/<int:pk>/', views.delete_category, name='delete_category'),
 ]
